fa461fc54914ccbf018a5f71782945af78e64869
xz <12345@qq.com>
2019-12-23 16:33:35 +0800

    完成用户列表的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,119 @@
 <template>
-  <el-button type="info" @click="logout">退出</el-button>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="flag ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggle">|||</div>
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="flag"  router :default-active="activePath">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+            <template slot="title">
+              <i :class="iconList[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveStorage('/' + subItem.path)">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      menuList: [],
+      iconList: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      flag: false,
+      activePath: ''
+    }
   },
-  created() {},
+  created() {
+    this.getList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+    },
+    toggle() {
+      this.flag = !this.flag
+    },
+    saveStorage(k) {
+      window.sessionStorage.setItem('activePath', k)
+      this.activePath = k
     }
   }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #333744;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  font-size: 26px;
+  color: #fff;
+  > div {
+    display: flex;
+    align-items: center;
+    > span {
+      padding-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
